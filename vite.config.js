7bb5e0e62fb330328380df55ba4b56b27d8c3257
deinsteins <hidayaatdenii@gmail.com>
2023-05-15 00:50:49 +0700

    fix production bug

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: 'src/main.scss',
-    },
     css: {
       preprocessorOptions: {
         scss: {
